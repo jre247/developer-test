04806ba659506bc23fe061558393a0c6b929ff8c
LAPTOP-VMM055E4\jevan <jevans8011@gmail.com>
2017-06-26 11:06:06 -0400

    Created unit test for testing property offer accept logic.

--- OrangeBricks.Web.Tests/Controllers/Property/Builders/PropertiesViewModelBuilderTest.cs
+++ OrangeBricks.Web.Tests/Controllers/Property/Builders/PropertiesViewModelBuilderTest.cs
@@ -5,6 +5,9 @@ using NSubstitute;
 using NUnit.Framework;
 using OrangeBricks.Web.Controllers.Property.Builders;
 using OrangeBricks.Web.Models;
+using System;
+using OrangeBricks.Web.Infrastructure;
+using Moq;
 
 namespace OrangeBricks.Web.Tests.Controllers.Property.Builders
 {
@@ -31,11 +34,18 @@ namespace OrangeBricks.Web.Tests.Controllers.Property.Builders
             _context = Substitute.For<IOrangeBricksContext>();
         }
 
+        private PropertiesViewModelBuilder GetPropertiesViewModelBuilder(string userId)
+        {
+            var mock = new Mock<IdentityManagerBase>();
+            mock.Setup(m => m.GetLoggedInUserId()).Returns(userId);
+            return new PropertiesViewModelBuilder(_context, mock.Object);
+        }
+
         [Test]
         public void BuildShouldReturnPropertiesWithMatchingStreetNamesWhenSearchTermIsProvided()
         {
             // Arrange
-            var builder = new PropertiesViewModelBuilder(_context);
+            var builder = GetPropertiesViewModelBuilder("user-id-1");
 
             var properties = new List<Models.Property>{
                 new Models.Property{ StreetName = "Smith Street", Description = "", IsListedForSale = true },
@@ -63,7 +73,7 @@ namespace OrangeBricks.Web.Tests.Controllers.Property.Builders
         public void BuildShouldReturnPropertiesWithMatchingDescriptionsWhenSearchTermIsProvided()
         {
             // Arrange
-            var builder = new PropertiesViewModelBuilder(_context);
+            var builder = GetPropertiesViewModelBuilder("user-id-1");
 
             var properties = new List<Models.Property>{
                 new Models.Property{ StreetName = "", Description = "Great location", IsListedForSale = true },
@@ -86,6 +96,50 @@ namespace OrangeBricks.Web.Tests.Controllers.Property.Builders
             // Assert
             Assert.That(viewModel.Properties.Count, Is.EqualTo(1));
             Assert.That(viewModel.Properties.All(p => p.Description.Contains("Town")));
+        }
+
+        [Test]
+        public void BuildShouldShowAcceptOfferMessageWhenOfferIsAccepted()
+        {
+            // Arrange
+            var userId = "user-id-1";
+            var builder = GetPropertiesViewModelBuilder(userId);
+            var now = DateTime.Now;
+            var properties = new List<Models.Property>{
+                new Models.Property{
+                    StreetName = "",
+                    Description = "Great location",
+                    IsListedForSale = true,
+                    Id = 123,
+                    Offers = new List<Models.Offer>
+                    {
+                        new Models.Offer
+                        {
+                            Status = OfferStatus.Accepted,
+                            UpdatedAt = now,
+                            Amount = 100,
+                            UserId = userId,
+                            Id = 1
+                        }
+                    }
+                },
+                new Models.Property{ StreetName = "", Description = "Town house", IsListedForSale = true }
+            };
+
+            var mockSet = Substitute.For<IDbSet<Models.Property>>()
+                .Initialize(properties.AsQueryable());
+
+            _context.Properties.Returns(mockSet);
+
+            var query = new PropertiesQuery();
+
+            // Act
+            var viewModel = builder.Build(query);
+
+            // Assert
+            var propertyToCompare = viewModel.Properties.ElementAt(0);
+            Assert.That(propertyToCompare.Offer.IsAccepted, Is.EqualTo(true));
+            Assert.That(propertyToCompare.Offer.AcceptDate, Is.EqualTo(now));
         }
     }
 }
