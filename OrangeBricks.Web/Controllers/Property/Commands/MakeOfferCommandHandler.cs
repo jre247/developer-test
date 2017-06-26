5892d3f871383bccf8c7dae70c2967ae232c30e3
LAPTOP-VMM055E4\jevan <jevans8011@gmail.com>
2017-06-26 09:38:42 -0400

    Setting UserId property for Offer model.

--- OrangeBricks.Web/Controllers/Property/Commands/MakeOfferCommandHandler.cs
+++ OrangeBricks.Web/Controllers/Property/Commands/MakeOfferCommandHandler.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections.Generic;
 using OrangeBricks.Web.Models;
+using Microsoft.AspNet.Identity;
+using OrangeBricks.Web.Infrastructure;
 
 namespace OrangeBricks.Web.Controllers.Property.Commands
 {
     public class MakeOfferCommandHandler
     {
         private readonly IOrangeBricksContext _context;
+        private IdentityManager _identityManager;
 
         public MakeOfferCommandHandler(IOrangeBricksContext context)
         {
             _context = context;
+            _identityManager = new IdentityManager();
         }
 
         public void Handle(MakeOfferCommand command)
@@ -22,7 +26,8 @@ namespace OrangeBricks.Web.Controllers.Property.Commands
                 Amount = command.Offer,
                 Status = OfferStatus.Pending,
                 CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
+                UpdatedAt = DateTime.Now,
+                UserId = _identityManager.GetLoggedInUserId()
             };
 
             if (property.Offers == null)
